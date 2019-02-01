10d512b59df7b355d65f8b32dc274f6aee022cc0
qaviton <qaviton@gmail.com>
2019-02-01 14:00:19 +0200

    adding examples

--- examples2.py
+++ examples2.py
@@ -10,15 +10,33 @@ import random
 # נדפיס את names_with_addresses_and_postal
 # נדפיס את postal_summary
 
-names_with_addresses = sort_names_with_addresses(get_data(30))
 
-for i in range(len(names_with_addresses)):
-    names_with_addresses[i] = (
-        names_with_addresses[0],
-        names_with_addresses[1],
-        str(random.randint(0, 10))+
-        str(random.randint(0, 10))+
-        str(random.randint(0, 10))+
-        str(random.randint(0, 10))+
-        str(random.randint(0, 10))
-    )
+def create_names_with_addresses_and_postal(names_with_addresses):
+    names_with_addresses_and_postal = []
+    for i in names_with_addresses:
+        names_with_addresses_and_postal.append((
+            i[0],
+            i[1],
+            str(random.randint(0, 10))+
+            str(random.randint(0, 10))+
+            str(random.randint(0, 10))+
+            str(random.randint(0, 10))+
+            str(random.randint(0, 10))
+        ))
+    return names_with_addresses_and_postal
+
+
+def sum_up_postal_codes_to_a_fat_number(names_with_addresses_and_postal):
+    postal = 0
+    for i in names_with_addresses_and_postal:
+        postal += int(i[2])
+    return postal
+
+
+names_with_addresses = sort_names_with_addresses(get_data(30))
+names_with_addresses_and_postal = create_names_with_addresses_and_postal(names_with_addresses)
+postal_sum = sum_up_postal_codes_to_a_fat_number(names_with_addresses_and_postal)
+
+print(names_with_addresses)
+print(names_with_addresses_and_postal)
+print(postal_sum)
