ea83a692161bb99441789a3cc6f52e451adece45
qaviton <qaviton@gmail.com>
2019-02-02 10:16:54 +0200

    improve rock paper scissors code

--- exercises/level_1/rock_paper_scissors.py
+++ exercises/level_1/rock_paper_scissors.py
@@ -18,47 +18,49 @@ def prepare_to_parse(word):
 choices = ["rock", "paper", "scissors"]
 
 
-while True:
-    player1 = "null"
-    player2 = "null"
+def play():
+    while True:
+        player1 = None
+        player2 = None
 
-    while player1 not in choices:
-        player1 = getpass("PLAYER ONE" + "\n" + "Rock, Paper, or Scissors? Choose! ")
-        player1 = prepare_to_parse(player1)
-        print("\n")
+        while player1 not in choices:
+            player1 = getpass("PLAYER ONE" + "\n" + "Rock, Paper, or Scissors? Choose! ")
+            player1 = prepare_to_parse(player1)
+            print("\n")
 
-    while player2 not in choices:
-        player2 = getpass("PLAYER TWO" + "\n" + "Rock, Paper, or Scissors? Choose! ")
-        player2 = prepare_to_parse(player2)
-        print("\n")
+        while player2 not in choices:
+            player2 = getpass("PLAYER TWO" + "\n" + "Rock, Paper, or Scissors? Choose! ")
+            player2 = prepare_to_parse(player2)
+            print("\n")
 
-    # This is the part that decides who wins.
-    while player1 == "rock":
-        if player2 == "rock":
-            print("It's a TIE! You're both losers.")
-        elif player2 == "paper":
-            print("Paper hugs rock! Player Two wins!")
-        elif player2 == "scissors":
-            print("Rock smashes Scissors! Player One wins!")
-        player1 = "null"
+        # This is the part that decides who wins.
+        if player1 == "rock":
+            if player2 == "rock":
+                print("It's a TIE! You're both losers.")
+            elif player2 == "paper":
+                print("Paper hugs rock! Player Two wins!")
+            elif player2 == "scissors":
+                print("Rock smashes Scissors! Player One wins!")
 
-    while player1 == "paper":
-        if player2 == "rock":
-            print("Paper hugs rock! Player One wins!")
-        elif player2 == "paper":
-            print("It's a TIE! You're both losers.")
-        elif player2 == "scissors":
-            print("Scissors cut Paper! Player Two wins!")
-        player1 = "null"
+        elif player1 == "paper":
+            if player2 == "rock":
+                print("Paper hugs rock! Player One wins!")
+            elif player2 == "paper":
+                print("It's a TIE! You're both losers.")
+            elif player2 == "scissors":
+                print("Scissors cut Paper! Player Two wins!")
 
-    while player1 == "scissors":
-        if player2 == "rock":
-            print("Rock smashes Scissors! Player Two wins!")
-        elif player2 == "paper":
-            print("Scissors cut Paper! Player One wins!")
-        elif player2 == "scissors":
-            print("It's a TIE! You're both losers.")
-        player1 = "null"
+        elif player1 == "scissors":
+            if player2 == "rock":
+                print("Rock smashes Scissors! Player Two wins!")
+            elif player2 == "paper":
+                print("Scissors cut Paper! Player One wins!")
+            elif player2 == "scissors":
+                print("It's a TIE! You're both losers.")
 
-    if input("Do you want to play another round? (y/n)") == "n":
-        break
+        if input("Do you want to play another round? (y/n)") == "n":
+            break
+
+
+if __name__ == "__main__":
+    play()
