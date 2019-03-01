03b7a31713b0281845058b174273e11763c05874
qaviton <qaviton@gmail.com>
2019-03-01 13:50:01 +0200

    adding exercises

--- flask_examples/simple_login_with_user_and_password.py
+++ flask_examples/simple_login_with_user_and_password.py
@@ -1,4 +1,3 @@
-import os
 import json
 from flask import Flask, render_template, request
 
@@ -6,29 +5,47 @@ from flask import Flask, render_template, request
 app = Flask('simple login app')
 
 
-app.route('/register', methods=['POST'])
+@app.route('/home', methods=['GET'])
+def home(message=''):
+    return render_template('login_or_register.html', message=message)
+
+
+@app.route('/register', methods=['POST'])
 def register():
     # get username & password from request
+    username = request.form['username']
+    password = request.form['password']
+
     # compare with database
+    with open('users.json') as f:
+        users = json.load(f)
+    if username in users:
+        return home(message='user already exist')
+
     # add to database if not exist
+    users[username] = {'password': password}
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
+
     # return response
-
-    # with open('users.json') as f:
-    #     users = json.load(f)
-    # # users['john'] = {'password': '123456'}
-    # # users['dror'] = {'password': '123456'}
-    # # users['domi'] = {'password': '123456'}
-    # # users['sunn'] = {'password': '123456'}
-    # #
-    # # with open('users.json', 'w') as f:
-    # #     json.dump(users, f)
-    # print(users)
-    pass
+    return home(message='register success')
 
 
-app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
     # get username & password from request
+    username = request.form['username']
+    password = request.form['password']
+
     # compare with database
-    # send authorized response
-    pass
+    with open('users.json') as f:
+        users = json.load(f)
+    if username not in users:
+        return home(message='user not found')
+    if password != users[username]['password']:
+        return home(message='wrong password')
+    return render_template('random_quote_template.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
