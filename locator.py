edc3a7372ffbb6abc2033b2c6f877d445c3719f6
yehonadav <yonadav.barilan@gmail.com>
2019-09-16 13:47:55 +0300

    bug fix added word counting

--- locator.py
+++ locator.py
@@ -8,4 +8,22 @@ import requests
 url = input("please enter a valid url:")
 
 response = requests.get('https://'+url)
-print(response.text)
+text = response.text
+valid_characters = "abcdefghijklmnopqrstuvwxyz"
+valid_characters += valid_characters.upper()
+
+for c in text:
+    if c not in valid_characters:
+        text = text.replace(c, ' ')
+words = text.split(' ')
+
+counter = {}
+for word in words:
+    if word not in counter:
+        counter[word] = 1
+    else:
+        counter[word] += 1
+
+
+for word, count in counter.items():
+    print(word, count)
