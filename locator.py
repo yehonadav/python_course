ee6ac73d0a9c35ff341a059047993f38f88e9acc
yehonadav <yonadav.barilan@gmail.com>
2019-09-16 12:49:33 +0300

    added a new branch for git lessons, this branch is supposed to be a hot fix kind of branch

---
+++ locator.py
@@ -0,0 +1,5 @@
+# create a program to fetch html content
+# from a selected website
+# print counters of re-occurring words
+# commit before each time you run this script
+# hint: use the requests module & input build-in function
