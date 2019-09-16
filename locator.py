ffebae2b5549b7d45a53370123591d2ee4237bc4
yehonadav <yonadav.barilan@gmail.com>
2019-09-16 13:35:41 +0300

    bug fix added url fetching support

--- locator.py
+++ locator.py
@@ -1,5 +1,11 @@
-# create a program to fetch html content
-# from a selected website
-# print counters of re-occurring words
-# commit before each time you run this script
-# hint: use the requests module & input build-in function
+# TODO: create a program to fetch html content
+# TODO: from a selected website
+# TODO: print counters of re-occurring words
+# TODO: commit before each time you run this script
+# TODO: hint: use the requests module & input build-in function
+import requests
+
+url = input("please enter a valid url:")
+
+response = requests.get('https://'+url)
+print(response.text)
