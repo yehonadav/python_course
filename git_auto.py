64a5406bff6e23eeb0616e93af64d8dc7fbd2073
yehonadav <yonadav.barilan@gmail.com>
2019-09-17 11:00:24 +0300

    added a new branch for git lessons, this branch is supposed to be a hot fix kind of branch

--- git_auto.py
+++ git_auto.py
@@ -1,5 +1,27 @@
 # TODO: create a new branch named auto
+# TODO: create a cred.py file with your username, password & email
+# TODO: add a cred.py file to .gitignore
 # TODO: create a git_auto.py file
 # TODO: install qaviton_git
 # TODO: create a script to automatically
-#  commit and push every 10 minutes
+#  commit and push every 10 seconds
+from qaviton_git import Git
+from cred import user, password, email
+from time import time, sleep
+
+
+git = Git(
+    password=password,
+    username=user,
+    email=email,
+)
+while True:
+    t = time()
+    try:
+        git.commit("auto commit")
+        git.push()
+    except:
+        pass
+    t = time() - t
+    if t < 10:
+        sleep(10-t)
